861f1053b39d81ebffa040054a62afee4b9e5988
repinaksenia <103584254+repinaksenia@users.noreply.github.com>
2022-05-04 14:51:05 +0500

    удалила файл event.js

--- js/event.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  document.querySelector('#event__btn').addEventListener('click', function() {
-    document.querySelector('#item3').classList.add('event__all-item')
-    document.querySelector('#item4').classList.add('event__all-item')
-    document.querySelector('#item5').classList.add('event__all-item')
-    document.querySelector('#event__btn').classList.add('hidden')
-  })
-  })
-
